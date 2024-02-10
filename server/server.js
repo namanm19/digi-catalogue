52fe4c641841a3f77c835eeda326db15bf2ea56d
Naman Maheshwari <84762148+namanm19@users.noreply.github.com>
2024-02-10 13:08:33 +0530

    new updates

--- server/server.js
+++ server/server.js
@@ -1,49 +1,48 @@
 // inorder to use process.env object we need this
-require('dotenv').config()
+require("dotenv").config();
 
 // creating an instance of express
-const express = require('express');
+const express = require("express");
 
 // creating an instance for mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // this line creates an express app and stored it in this constant
 // basically initializes the express
 const app = express();
-const catelogRoutes = require('../server/routes/catelogRoutes');
+const catelogRoutes = require("../server/routes/catelogRoutes");
 
 // process.env object contains all the environment variables of .env file
 // process.env object is available globally in nodejs environment
 const PORT = process.env.PORT | 4000;
-
 
 // middle ware
 // this checks if the request from the client side has some body or data attached to it
 // if yes then that body/data is stored in req object
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method);
+  next();
 });
 
-// routes 
+// routes
 // connected to all the routes in workouts.js file
 // be sending a GET,POST,DELETE,PATCH request to our server
 // we will be using this route /api/workouts[ENDPOINT] in order to send request to server
-app.use('/api/catelogs', catelogRoutes);
-
+app.use("/api/catelogs", catelogRoutes);
 
 // connecting to mongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        // listen for requests
-        app.listen(PORT, () => {
-            console.log("connected to DB & listen for port: ", PORT);
-        });
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log("connected to DB & listen for port: ", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // calling localhost:4000/ will send a get request to localhost:4000 server and
 // server sends a response back for this request with this message
@@ -51,10 +50,8 @@ mongoose.connect(process.env.MONGO_URI)
 //     res.json({msg: "welcome to the app"})
 // })
 
-
-
 // to run server without nodemon
-// > node server.js OR USE 
+// > node server.js OR USE
 // > npm start
 // to run server with nodemon
 // > npm run dev
